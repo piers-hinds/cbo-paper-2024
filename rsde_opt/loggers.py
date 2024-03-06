da0af6f1738c4904ecd5a4de0e119cdb74bfd428
Piers <61315972+piers-hinds@users.noreply.github.com>
2024-03-06 15:53:23 +0000

    updated animation

--- rsde_opt/loggers.py
+++ rsde_opt/loggers.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+from matplotlib.animation import FuncAnimation, PillowWriter
 
 
 class ParticleSystemLogger:
@@ -14,23 +14,38 @@ class ParticleSystemLogger:
         self.path = torch.cat([self.path, state.unsqueeze(0)])
         self.consensus_path = torch.cat([self.consensus_path, consensus.unsqueeze(0)])
 
-    def animate_particles(self):
-        fig, ax = plt.subplots()
-        scatter = ax.scatter([], [], animated=True, s=1)
-
-        def init():
+    def animate_particles(self, ax=None):
+        if ax is None:
+            fig, ax = plt.subplots()
             ax.set_xlim(self.path[:, :, 0].min(), self.path[:, :, 0].max())
             ax.set_ylim(self.path[:, :, 1].min(), self.path[:, :, 1].max())
-            return scatter,
+        else:
+            fig = ax.figure
+
+        # Particle scatter plot
+        scatter = ax.scatter([], [], animated=True, s=1, color='blue', label='Particles')
+
+        # Consensus scatter plot, different color and labeled
+        consensus_scatter = ax.scatter([], [], animated=True, s=20, color='red', label='Consensus')
+
+        def init():
+            ax.legend(loc='upper left')
+            return scatter, consensus_scatter,
 
         def update(frame):
             x_data = self.path[frame, :, 0].numpy()
             y_data = self.path[frame, :, 1].numpy()
             scatter.set_offsets(np.column_stack([x_data, y_data]))
-            return scatter,
+
+            # Update consensus position
+            consensus_x = self.consensus_path[frame, 0].numpy()
+            consensus_y = self.consensus_path[frame, 1].numpy()
+            consensus_scatter.set_offsets(np.array([[consensus_x, consensus_y]]))
+
+            return scatter, consensus_scatter,
+
         frames = self.path.shape[0]
         interval = 2000 / frames
-        ani = FuncAnimation(fig, update, frames=frames,
-                            init_func=init, blit=True, interval=interval)
-
+        ani = FuncAnimation(fig, update, frames=frames, init_func=init, blit=True, interval=interval)
         plt.show()
+        return ani
