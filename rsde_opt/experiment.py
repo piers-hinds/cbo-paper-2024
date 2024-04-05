784918b3479c1b7d4a509327734567828ef09132
Piers <61315972+piers-hinds@users.noreply.github.com>
2024-04-05 16:02:11 +0100

    experiment loggers

--- rsde_opt/experiment.py
+++ rsde_opt/experiment.py
@@ -1,13 +1,15 @@
 from tqdm.auto import tqdm
 import torch
 from .particle_system import ParticleSystem
+from .loggers import ExperimentLogger
 
 
 def run_experiment(system: ParticleSystem,
                    num_steps: int,
                    true_optimum: torch.tensor,
                    epsilon: float,
-                   num_runs: int) -> float:
+                   num_runs: int,
+                   logger: ExperimentLogger = None) -> float:
     """
     Run the particle system experiment multiple times and calculate the success rate.
 
@@ -17,28 +19,34 @@ def run_experiment(system: ParticleSystem,
         true_optimum: The true optimal objective value.
         epsilon: The tolerance for considering a run successful.
         num_runs: The number of times to run the experiment.
+        logger: An option logger to record the particles consensus
 
     Returns:
         The success rate of the experiment.
     """
     success_count = 0
 
-    pbar = tqdm(range(num_runs))
-    pbar.set_description("N={}, ".format(system.num_particles) +
-                         "alpha={:.1f}, ".format(system.alpha) +
-                         "beta={:.1f}, ".format(system.beta) +
-                         "sigma={:.1f} ".format(system.sigma)
-                         )
+    progress_bar = tqdm(range(num_runs))
+    progress_bar.set_description("N={}, ".format(system.num_particles) +
+                                 "alpha={:.1f}, ".format(system.alpha) +
+                                 "beta={:.1f}, ".format(system.beta) +
+                                 "sigma={:.1f} ".format(system.sigma)
+                                 )
 
-    for i in pbar:
+    for i in progress_bar:
         for _ in range(num_steps):
             normals = torch.randn_like(system.state, device=system.device)
-            state, _ = system.step(normals)
+            state, consensus = system.step(normals)
+
+            if logger is not None:
+                logger.log_consensus(experiment_index=i, consensus=consensus)
+                objective_value = system.objective(consensus.unsqueeze(0))
+                logger.log_objective(experiment_index=i, objective_value=objective_value)
 
         if torch.linalg.norm(system.consensus() - true_optimum) < epsilon:
             success_count += 1
 
         system.reset()
 
-        pbar.set_postfix({'Success rate': success_count / (i+1)})
+        progress_bar.set_postfix({'Success rate': success_count / (i+1)})
     return success_count / num_runs
--- rsde_opt/loggers.py
+++ rsde_opt/loggers.py
@@ -10,3 +10,18 @@ class ParticleSystemLogger:
     def log_state(self, state, consensus):
         self.path = torch.cat([self.path, state.unsqueeze(0)])
         self.consensus_path = torch.cat([self.consensus_path, consensus.unsqueeze(0)])
+
+
+class ExperimentLogger:
+    def __init__(self, num_experiments):
+        self.num_experiments = num_experiments
+        self.consensus_paths = [torch.empty(0) for _ in range(num_experiments)]
+        self.objective_values = [torch.empty(0) for _ in range(num_experiments)]
+
+    def log_consensus(self, experiment_index, consensus):
+        self.consensus_paths[experiment_index] = torch.cat([self.consensus_paths[experiment_index],
+                                                            consensus.unsqueeze(0)])
+
+    def log_objective(self, experiment_index, objective_value):
+        self.objective_values[experiment_index] = torch.cat([self.objective_values[experiment_index],
+                                                             objective_value])
