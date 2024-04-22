98134726f77b580b6ee092ebf01ecc97254599b6
Piers <61315972+piers-hinds@users.noreply.github.com>
2024-04-22 10:42:41 +0100

    Success criterion and tests

--- rsde_opt/experiment.py
+++ rsde_opt/experiment.py
@@ -4,29 +4,59 @@ from .particle_system import ParticleSystem
 from .loggers import ExperimentLogger
 
 
+class SuccessCriterion:
+    def __init__(self,
+                 true_optimum: torch.Tensor,
+                 epsilon: float,
+                 optimum_type: str):
+        """
+        Args:
+            true_optimum: The true optimum of the optimization problem. Can be the coordinates of the location or the
+                function value.
+            epsilon: The tolerance level.
+            optimum_type: One of 'objective_value' or 'x_value' depending on whether the true optimum given is the
+                objective function value or the location (x value).
+        """
+        optimum_types = ['objective_value', 'x_value']
+        if optimum_type not in optimum_types:
+            raise ValueError("Invalid optimum_type value. Expected one of: %s" % optimum_types)
+        self.true_optimum = true_optimum
+        self.epsilon = epsilon
+        self.optimum_type = optimum_type
+
+    def check(self, consensus: torch.Tensor, objective: torch.Tensor):
+        if self.optimum_type == 'objective_value':
+            return objective - self.true_optimum < self.epsilon
+
+        if self.optimum_type == 'x_value':
+            assert consensus.shape == self.true_optimum.shape, \
+                "Success check failed: shape of true optimum does not match consensus shape"
+            return torch.linalg.norm(consensus - self.true_optimum) < self.epsilon
+
+
 def run_experiment(system: ParticleSystem,
                    num_steps: int,
-                   true_optimum: torch.tensor,
-                   epsilon: float,
+                   success_criterion: SuccessCriterion,
                    num_runs: int,
-                   logger: ExperimentLogger = None) -> float:
+                   logger: ExperimentLogger = None,
+                   progress_bar: bool = True) -> float:
     """
     Run the particle system experiment multiple times and calculate the success rate.
 
     Args:
         system: The particle system to run the experiment on.
         num_steps: The number of iterations of the numerical scheme.
-        true_optimum: The true optimal objective value.
-        epsilon: The tolerance for considering a run successful.
+        success_criterion: A SuccessCriterion object to determine if an experiment is successful.
         num_runs: The number of times to run the experiment.
         logger: An option logger to record the particles consensus
+        progress_bar: If True, a progress bar is displayed
 
     Returns:
         The success rate of the experiment.
     """
     success_count = 0
 
-    progress_bar = tqdm(range(num_runs))
+    progress_bar = tqdm(range(num_runs), disable=not progress_bar)
     progress_bar.set_description("N={}, ".format(system.num_particles) +
                                  "alpha={:.1f}, ".format(system.alpha) +
                                  "beta={:.1f}, ".format(system.beta) +
@@ -43,7 +73,10 @@ def run_experiment(system: ParticleSystem,
                 objective_value = system.objective(consensus.unsqueeze(0))
                 logger.log_objective(experiment_index=i, objective_value=objective_value)
 
-        if torch.linalg.norm(system.consensus() - true_optimum) < epsilon:
+        final_consensus = system.consensus().cpu()
+        final_objective = system.objective(final_consensus.unsqueeze(0)).cpu()
+
+        if success_criterion.check(final_consensus, final_objective):
             success_count += 1
 
         system.reset()
---
+++ tests/test_experiment.py
@@ -0,0 +1,83 @@
+from rsde_opt.experiment import SuccessCriterion, run_experiment
+from rsde_opt.particle_system import SimpleProjectionParticleSystem
+from rsde_opt.loggers import ExperimentLogger
+import torch
+import pytest
+
+
+@pytest.fixture
+def example_simple_proj_particle_system():
+    def unit_clamp(x):
+        x.clamp(-1, 1)
+
+    eps = SimpleProjectionParticleSystem(objective=lambda x: torch.sum(x ** 2, dim=1),
+                                         projection=unit_clamp,
+                                         initial_state=lambda n: torch.zeros(n, 2),
+                                         alpha=1,
+                                         beta=1,
+                                         sigma=4,
+                                         dim=2,
+                                         num_particles=5,
+                                         step_size=0.01)
+
+    return eps
+
+
+def test_success_criterion_obj_init():
+    sc = SuccessCriterion(torch.tensor(0.),
+                          0.1,
+                          'objective_value')
+
+
+def test_success_criterion_x_init():
+    sc = SuccessCriterion(torch.zeros(2),
+                          0.1,
+                          'x_value')
+
+
+def test_success_criterion_obj_check():
+    sc = SuccessCriterion(torch.tensor(0.),
+                          0.1,
+                          'objective_value')
+    assert sc.check(torch.zeros(2), torch.tensor(0.01))
+
+
+def test_success_criterion_x_check():
+    sc = SuccessCriterion(torch.zeros(2),
+                          0.1,
+                          'x_value')
+    assert sc.check(torch.zeros(2) + 0.001, torch.tensor(0.01))
+
+
+def test_success_criterion_x_check_false():
+    sc = SuccessCriterion(torch.zeros(2),
+                          0.1,
+                          'x_value')
+    assert not sc.check(torch.zeros(2) + 10, torch.tensor(0.01))
+
+
+def test_run_experiment(example_simple_proj_particle_system):
+    sc = SuccessCriterion(torch.tensor(0.),
+                          0.1,
+                          'objective_value')
+    out = run_experiment(example_simple_proj_particle_system,
+                         10,
+                         sc,
+                         5,
+                         progress_bar=False)
+    assert 0 <= out <= 1
+
+
+def test_run_experiment_logger(example_simple_proj_particle_system):
+    sc = SuccessCriterion(torch.tensor(0.),
+                          0.1,
+                          'objective_value')
+    n = 5
+    logger = ExperimentLogger(n)
+    out = run_experiment(example_simple_proj_particle_system,
+                         10,
+                         sc,
+                         n,
+                         logger=logger,
+                         progress_bar=False)
+    assert 0 <= out <= 1
