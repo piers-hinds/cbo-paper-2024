582aea590bf60055b4265594ccb9d458693dc616
Piers <61315972+piers-hinds@users.noreply.github.com>
2024-12-05 16:56:59 +0000

    added optional colour bar, log scale, and true optimum for create_ax

--- rsde_opt/visualization.py
+++ rsde_opt/visualization.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.ticker import LogFormatterSciNotation, LogLocator
 import torch
 
 
@@ -109,4 +110,62 @@ def create_ax(objective, feasible_region, x_lim, y_lim):
 
     z_objective_masked = np.where(z_feasible <= 0, z_objective, np.nan)
     ax.contourf(x_grid, y_grid, z_objective_masked, levels=20, cmap='viridis', alpha=0.5)
+    return ax
+
+
+def create_ax(objective, feasible_region, x_lim, y_lim, colorbar=False, log_scale=False, optimum=None):
+    """
+    Creates a visualization of the objective function constrained by the feasible region.
+
+    Args:
+        objective (callable): The objective function to visualize.
+        feasible_region (callable): A function defining the feasibility of a point.
+                                    Should take x, y arrays and return a feasibility mask.
+        x_lim (tuple): Limits for the x-axis.
+        y_lim (tuple): Limits for the y-axis.
+        colorbar (bool): Whether to display a color bar for the objective values. Default is False.
+        log_scale (bool): Whether to use logarithmic scaling for contour levels. Default is False.
+        optimum (np.ndarray, optional): A NumPy array representing the optimum point to be plotted.
+
+    Returns:
+        matplotlib.axes.Axes: The axes object containing the plot.
+    """
+    x = np.linspace(x_lim[0], x_lim[1], 400)
+    y = np.linspace(y_lim[0], y_lim[1], 400)
+    x_grid, y_grid = np.meshgrid(x, y)
+    points = np.stack((x_grid.flatten(), y_grid.flatten()), axis=-1)
+
+    z_feasible = feasible_region(x_grid, y_grid)
+
+    points_tensor = torch.tensor(points, dtype=torch.float32)
+    z_objective = objective(points_tensor).numpy().reshape(x_grid.shape)
+
+    fig, ax = plt.subplots()
+
+    ax.contour(x_grid, y_grid, z_feasible, levels=[0, 0.001], cmap='Blues', alpha=1)
+
+    z_objective_masked = np.where(z_feasible <= 0, z_objective, np.nan)
+
+    # Determine contour levels
+    if log_scale:
+        min_val = np.nanmin(z_objective_masked[z_objective_masked > 0])
+        max_val = np.nanmax(z_objective_masked)
+        levels = np.geomspace(min_val, max_val, 20)
+    else:
+        levels = 20
+
+    contour = ax.contourf(x_grid, y_grid, z_objective_masked, levels=levels, cmap='viridis', alpha=0.5)
+
+    if colorbar:
+        cbar = fig.colorbar(contour, ax=ax)
+        if log_scale:
+            min_val = np.nanmin(z_objective_masked[z_objective_masked > 0])
+            max_val = np.nanmax(z_objective_masked)
+            ticks = [10 ** i for i in range(int(np.floor(np.log10(min_val))), int(np.ceil(np.log10(max_val))) + 1)]
+            cbar.set_ticks(ticks[1:-1])
+            cbar.ax.yaxis.set_major_formatter(LogFormatterSciNotation())
+
+    if optimum is not None:
+        ax.plot(optimum[0], optimum[1], 'ro', markersize=6)
+
     return ax
